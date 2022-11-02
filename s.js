5898297ee83c32960bb2f0a695151a1170d8516a
Harshanl2002 <harshan.l@outlook.com>
2022-11-02 21:13:09 +0530

    initial commit

---
+++ s.js
@@ -0,0 +1,26 @@
+const i = document.getElementById("input");
+const b = document.getElementById("btn-1");
+const ul = document.getElementById("ulid");
+var link = [];
+b.onclick = function () {
+    link.push(i.value);
+    i.value = "";
+    console.log(link);
+    p()
+}
+i.addEventListener("keydown", function (event) {
+    //console.log(event.which);
+    if (event.which === 13) {
+        b.click();
+    }
+});
+function p() {
+    let ele = "";
+    for (let i1 = 0; i1 < link.length; i1++) {
+        ele += `<li><a href='${link[i1]}' target='_blank'>${link[i1]}</a></li>`;
+    }
+    ul.innerHTML = ele;
+    console.log(ele);
+}
+
+
