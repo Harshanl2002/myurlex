18afff4ec68a4715f9ddaa74936b83ea0bb2843c
Harshanl2002 <harshan.l@outlook.com>
2022-11-03 10:33:07 +0530

    delete btn added

--- s.js
+++ s.js
@@ -2,6 +2,19 @@ const i = document.getElementById("input");
 const b = document.getElementById("btn-1");
 const ul = document.getElementById("ulid");
 var link = []
+var locallinks
+var d = document.getElementById("Delete")
+locallinks = JSON.parse(localStorage.getItem("links"))
+console.log(locallinks);
+if (locallinks) {
+    link = locallinks
+    p();
+}
+d.ondblclick = () => {
+    localStorage.clear()
+    link = []
+    p();
+}
 b.onclick = function () {
     link.push(i.value);
     i.value = "";
@@ -18,7 +31,6 @@ i.addEventListener("keydown", function (event) {
 });
 function p() {
     let ele = "";
-    link = JSON.parse(link);
     for (let i1 = 0; i1 < link.length; i1++) {
         ele += `<li><a href='${link[i1]}' target='_blank'>${link[i1]}</a></li>`;
     }
